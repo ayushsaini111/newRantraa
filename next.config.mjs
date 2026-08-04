1c117c2d65036f7c32e585e0e4130bc0e44284c5
divyanshinvester <divyanshmishra.investor@gmail.com>
2026-08-04 17:47:32 +0530

    first13

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+// frontend/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -7,17 +8,26 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
+        port: "",
         pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
+        port: "",
         pathname: "/dl79knb0g/**",
       },
     ],
   },
 
-
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: "https://astro-nine-beige.vercel.app/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
