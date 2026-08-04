6cd806344f023d67edc5434a5159e050fa51630e
divyanshinvester <divyanshmishra.investor@gmail.com>
2026-08-04 16:11:33 +0530

    first4

--- next.config.mjs
+++ next.config.mjs
@@ -18,19 +18,7 @@ const nextConfig = {
     ],
   },
 
-  async rewrites() {
-    const backendUrl =
-      process.env.NODE_ENV === "production"
-        ? "https://astro-nine-beige.vercel.app"
-        : "http://localhost:3001";
-
-    return [
-      {
-        source: "/backend/:path*",
-        destination: `${backendUrl}/api/:path*`,
-      },
-    ];
-  },
+ 
 };
 
 export default nextConfig;
